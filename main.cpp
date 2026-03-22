aa1d34b73542bf1ee6897bc9ba455c052821691f
Wilmar Smit <u24584216@tuks.co.za>
2026-03-22 12:53:55 +0200

    updated main slightly

--- main.cpp
+++ main.cpp
@@ -190,7 +190,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	const char *baseWindowTitle = "OpenGL 3.3 Window";
+	const char *baseWindowTitle = "u24584216";
 	GLFWwindow *window = glfwCreateWindow(1200, 800, baseWindowTitle, NULL, NULL);
 
 	if (!window)
